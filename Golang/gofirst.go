018cca6a4e44ea6d4eeffcd1d5627942c4584ee8
Narenderreddygopu <narendergopu25@gmail.com>
2023-06-30 01:51:04 -0500

    Initial commit

---
+++ Golang/gofirst.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var student1 string = "Narender" //type is string
+	var student2 string = "Ramya"    //type is inferred
+	var student3 string = "Uthej"
+	var student4 string = "Harish"
+	x := 4 //type is inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(student3)
+	fmt.Println(student4)
+	fmt.Println(x)
+}
