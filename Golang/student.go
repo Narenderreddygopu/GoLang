7962f12ff97f8875cfa6651c72db5b8bf6f55c22
Narenderreddygopu <narendergopu25@gmail.com>
2023-07-04 00:56:24 -0500

    initial commit4

--- Golang/student.go
+++ Golang/student.go
@@ -7,50 +7,50 @@ import (
 
 func main() {
 	var start_time = time.Now()
-	var array1 = [5]int{}
-	// var total_students_count int = 4
-	// var narender int
-	// var student2 int
-	// var student3 int
-	// var student4 int
-	var avg_marks int
+	var subject_count int
+	// var student_userId int
+	var students_count int
+	// fmt.Print("Enter student Id :")
+	// fmt.Scanln(&Student_userId)
+	fmt.Printf("Enter number of subjects :")
+	fmt.Scanln(&subject_count)
+	fmt.Printf("Enter number of students :")
+	fmt.Scanln(&students_count)
+	var array1 = make([]int, subject_count)
+	var array2 = make([]int, students_count)
+	var array3 = make([]int, students_count)
 	var sum_of_marks int
-	// x := 4 //type is inferred
+	//var avg_marks int
+	for i := 0; i < students_count; i++ {
+		fmt.Print("Enter student ", i+1, "ID :")
+		fmt.Scanln(&array2[i])
+		for j := 0; j < subject_count; j++ {
 
-	// fmt.Print("Enter narender1 marks: ")
-	// fmt.Scanln(&narender)
-
-	// fmt.Print("Enter student2 marks: ")
-	// fmt.Scanln(&student2)
-
-	// fmt.Print("Enter student3 marks: ")
-	// fmt.Scanln(&student3)
-
-	// fmt.Print("Enter student4 marks: ")
-	// fmt.Scanln(&student4)
-
-	for i := 1; i < 5; i++ {
-		fmt.Print("Enter Student", i, " marks: ")
-		fmt.Scanln(&array1[i])
-		sum_of_marks = sum_of_marks + array1[i]
+			fmt.Print("Enter Subject ", j+1, " marks: ")
+			fmt.Scanln(&array1[j])
+			sum_of_marks = sum_of_marks + array1[i]
+		}
+		array3[i] = sum_of_marks / subject_count
+		sum_of_marks = 0
 	}
+	// avg_marks = (sum_of_marks) / subject_count
+	for i := 0; i < students_count; i++ {
 
-	// sum_of_marks = (narender + student2 + student3 + student4)
-	avg_marks = (sum_of_marks) / 4
+		switch {
+		case array3[i] >= 90:
+			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with A Garde ")
+		case array3[i] >= 80 && array3[i] < 90:
+			fmt.Print("\nstudent ", i+1, " secured  is  :", array3[i], " with B Garde ")
+		case array3[i] >= 70 && array3[i] < 80:
+			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with C Garde ")
+		case array3[i] >= 60 && array3[i] < 70:
+			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with D Garde ")
+		case array3[i] >= 50 && array3[i] < 60:
+			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with E Garde ")
+		case array3[i] < 50:
+			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with F Garde ")
+		}
+	}
 	elapsed := time.Since(start_time)
-	// fmt.Println(narender)
-	// fmt.Println(student2)
-	// fmt.Println(student3)
-	// fmt.Println(student4)
-	// fmt.Println(x)
-
-	// fmt.Printf("%d + %d + %d + %d = %d\n", narender, student2, student3, student4, avg_marks)
-
-	fmt.Println("Avg marks of students : ", avg_marks)
-
-	/*output*/
-	// fmt.Println("total_students_count", total_students_count)
-
-	fmt.Printf("%s", elapsed)
-
+	fmt.Printf("\n%s", elapsed)
 }
