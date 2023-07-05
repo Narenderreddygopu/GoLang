df523d3a3674fa94526c49db4c52da8f7ddd3c37
narendereddygopu <narendereddy25@gmail.com>
2023-07-05 11:57:43 +0530

    updated code with student and teacher login

--- Golang/student.go
+++ Golang/student.go
@@ -1,56 +1,101 @@
 package main
-
 import (
 	"fmt"
 	"time"
 )
 
-func main() {
-	var start_time = time.Now()
+type Student struct {
+	Email string
+	Password string
+}
+
+type Teacher struct {
+	Email string
+	Password string
+}
+
+func TeacherWork(){
 	var subject_count int
-	// var student_userId int
 	var students_count int
-	// fmt.Print("Enter student Id :")
-	// fmt.Scanln(&Student_userId)
 	fmt.Printf("Enter number of subjects :")
 	fmt.Scanln(&subject_count)
 	fmt.Printf("Enter number of students :")
 	fmt.Scanln(&students_count)
-	var array1 = make([]int, subject_count)
-	var array2 = make([]int, students_count)
-	var array3 = make([]int, students_count)
+	var SubjectCount = make([]int, subject_count)
+	var StudentArray = make([]int, students_count)
+	var StudentPercentArray = make([]int, students_count)
 	var sum_of_marks int
-	//var avg_marks int
 	for i := 0; i < students_count; i++ {
 		fmt.Print("Enter student ", i+1, "ID :")
-		fmt.Scanln(&array2[i])
+		fmt.Scanln(&StudentArray[i])
 		for j := 0; j < subject_count; j++ {
-
 			fmt.Print("Enter Subject ", j+1, " marks: ")
-			fmt.Scanln(&array1[j])
-			sum_of_marks = sum_of_marks + array1[i]
+			fmt.Scanln(&SubjectCount[j])
+			sum_of_marks = sum_of_marks + SubjectCount[i]
 		}
-		array3[i] = sum_of_marks / subject_count
+		StudentPercentArray[i] = sum_of_marks / subject_count
 		sum_of_marks = 0
 	}
-	// avg_marks = (sum_of_marks) / subject_count
 	for i := 0; i < students_count; i++ {
-
 		switch {
-		case array3[i] >= 90:
-			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with A Garde ")
-		case array3[i] >= 80 && array3[i] < 90:
-			fmt.Print("\nstudent ", i+1, " secured  is  :", array3[i], " with B Garde ")
-		case array3[i] >= 70 && array3[i] < 80:
-			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with C Garde ")
-		case array3[i] >= 60 && array3[i] < 70:
-			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with D Garde ")
-		case array3[i] >= 50 && array3[i] < 60:
-			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with E Garde ")
-		case array3[i] < 50:
-			fmt.Print("\nstudent ", i+1, " secured % is  :", array3[i], " with F Garde ")
+			case StudentPercentArray[i] >= 90:
+				fmt.Print("\nstudent ", i+1, " secured a percentage of  :", StudentPercentArray[i], "% and received an A grade.")
+			case StudentPercentArray[i] >= 80 && StudentPercentArray[i] < 90:
+				fmt.Print("\nstudent ", i+1, " secured a percentage of  :", StudentPercentArray[i], "% and received an B grade.")
+			case StudentPercentArray[i] >= 70 && StudentPercentArray[i] < 80:
+				fmt.Print("\nstudent ", i+1, " secured a percentage of  :", StudentPercentArray[i], "% and received an C grade.")
+			case StudentPercentArray[i] >= 60 && StudentPercentArray[i] < 70:
+				fmt.Print("\nstudent ", i+1, " secured a percentage of  :", StudentPercentArray[i], "% and received an D grade.")
+			case StudentPercentArray[i] >= 50 && StudentPercentArray[i] < 60:
+				fmt.Print("\nstudent ", i+1, " secured a percentage of  :", StudentPercentArray[i], "% and received an E grade.")
+			case StudentPercentArray[i] < 50:
+				fmt.Print("\nstudent ", i+1, " secured a percentage of  :", StudentPercentArray[i], "% and received an F grade.")
 		}
 	}
+}
+
+func main() {
+	var start_time = time.Now()
+	fmt.Printf("Hello Welcome to ABC Services : \n")
+	fmt.Printf("1. Login as Student\n")
+	fmt.Printf("2. Login as Teacher\nEnter 1 or 2 as input: ")
+	var logininp int
+	var Emailinp string
+	var Passwordinp string
+	var student1 Student
+	var teacher1 Teacher
+	student1.Email = "bob@gmail.com"
+	student1.Password = "bob@123"
+	teacher1.Email = "alice@gmail.com"
+	teacher1.Password = "alice@123"
+	fmt.Scanln(&logininp)
+	
+	if logininp == 1 {
+		fmt.Printf("Enter Email: ")
+		fmt.Scanln(&Emailinp)
+		fmt.Printf("Enter Password: ")
+		fmt.Scanln(&Passwordinp)
+		if Emailinp == student1.Email && Passwordinp == student1.Password{
+			fmt.Printf("\nSuccessfull Login")
+		}else{
+			fmt.Print("\nError in login. Please try again.")
+		}
+	}else if logininp == 2 {
+		fmt.Printf("Enter Email: ")
+		fmt.Scanln(&Emailinp)
+		fmt.Printf("Enter Password: ")
+		fmt.Scanln(&Passwordinp)
+		if Emailinp == teacher1.Email && Passwordinp == teacher1.Password{
+			fmt.Printf("\nSuccessfull login\n")
+			TeacherWork()
+		}else{
+			fmt.Print("\nError in login. Please try again.")
+		}
+	}else{
+		fmt.Printf("\nInvalid input.Please enter either 1 or 2")
+	}
+
+
 	elapsed := time.Since(start_time)
 	fmt.Printf("\n%s", elapsed)
 }
